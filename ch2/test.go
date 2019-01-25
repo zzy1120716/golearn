45fe3dc9f3f3ffc169d96fdc6a34c6eb6678fe69
zzy <zzy1120716@126.com>
2019-01-25 16:25:54 +0800

    test ch2

--- ch2/test.go
+++ ch2/test.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"golearn/ch2/tempconv"
-)
+import "fmt"
 
 /*var p = f()
 
@@ -74,8 +71,47 @@ func main() {
 	/*medals := []string{"gold", "silver", "bronze"}
 	fmt.Println(medals)*/
 
-	fmt.Printf("Brrrr! %v\n", tempconv.AbsoluteZero)
+	/*fmt.Printf("Brrrr! %v\n", tempconv.AbsoluteZero)
 	fmt.Println(tempconv.CToF(tempconv.BoilingC))
 	fmt.Println(tempconv.FToC(tempconv.CToF(tempconv.BoilingC)))
-	fmt.Printf("%v\n", tempconv.CToK(tempconv.AbsoluteZero))
+	fmt.Printf("%v\n", tempconv.CToK(tempconv.AbsoluteZero))*/
+
+	//p := new(int)
+	//q := new(int)
+	//fmt.Println(p == q)		// false
+
+	//var x uint8 = 1<<1 | 1<<5	// 100010
+	//var y uint8 = 1<<1 | 1<<2	// 110
+	//fmt.Printf("%08b\n", x)	// 00100010，二进制8位，不足补0
+	//fmt.Printf("%08b\n", y)	// 00000110
+	//fmt.Printf("%08b\n", x&y)	// 00000010
+	//fmt.Printf("%08b\n", x|y)	// 00100110
+	//fmt.Printf("%08b\n", x^y)	// 00100100
+	//fmt.Printf("%08b\n", x&^y)	// 00100000
+	//
+	//for i := uint(0); i < 8; i++ {
+	//	if x&(1<<i) != 0 {
+	//		fmt.Println(i)	// 1	5
+	//	}
+	//}
+	//
+	//fmt.Printf("%08b\n", x<<1)	// 01000100
+	//fmt.Printf("%08b\n", x>>1)	// 00010001
+
+	/*medals := []string{"gold", "silver", "bronze"}
+	for i := len(medals) - 1; i >= 0; i-- {
+		fmt.Println(medals[i])	// "bronze", "silver", "gold"
+	}*/
+
+	//o := 0666
+	//fmt.Printf("%d %[1]o %#[1]o\n", o)
+	//x := int64(0xdeadbeef)
+	//fmt.Printf("%d %[1]x %#[1]x %#[1]X\n", x)
+
+	ascii := 'a'
+	unicode := '国'
+	newline := '\n'
+	fmt.Printf("%d %[1]c %[1]q\n", ascii)   // "97 a 'a'"
+	fmt.Printf("%d %[1]c %[1]q\n", unicode) // "22269 国 '国'"
+	fmt.Printf("%d %[1]q\n", newline)       // "10 '\n'"
 }
